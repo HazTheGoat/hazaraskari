eca545a4872514e90834bfa13f9cb2eda26460fc
Hazar Askari <hazar.askari@miros-group.com>
2025-03-13 10:53:04 +0100

    update motion

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -294,7 +294,6 @@ export function App() {
               <motion.div
                 className="mb-80"
                 initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1.5 }}
                 viewport={{ once: true }}
               >
--- src/app/components/particles.tsx
+++ src/app/components/particles.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { loadSlim } from '@tsparticles/slim';
 import type { Engine } from '@tsparticles/engine';
+import { motion } from 'framer-motion';
 
 export const Animation = () => {
   const [init, setInit] = useState(false);
@@ -20,81 +21,87 @@ export const Animation = () => {
 
   if (init) {
     return (
-      <Particles
-        particlesLoaded={particlesLoaded}
-        options={{
-          fullScreen: {
-            enable: false,
-          },
-          particles: {
-            number: {
-              value: 650, // number of particles
-              density: {
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}
+      >
+        <Particles
+          particlesLoaded={particlesLoaded}
+          options={{
+            fullScreen: {
+              enable: false,
+            },
+            particles: {
+              number: {
+                value: 650, // number of particles
+                density: {
+                  enable: true,
+                  height: 400,
+                  width: 400,
+                },
+              },
+              shape: {
+                type: '',
+              },
+              links: {
                 enable: true,
-                height: 400,
-                width: 400,
+                distance: 20, // distance between particles
+                color: '#39FF14', // color of lines
+                opacity: 1,
+                width: 1,
               },
-            },
-            shape: {
-              type: '',
-            },
-            links: {
-              enable: true,
-              distance: 20, // distance between particles
-              color: '#39FF14', // color of lines
-              opacity: 1,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 0.7,
-              direction: 'none',
-              random: false,
-              straight: false,
-              outModes: {
-                default: 'out',
-              },
-              attract: {
-                enable: false,
-                rotate: {
-                  x: 600,
-                  y: 1200,
+              move: {
+                enable: true,
+                speed: 0.7,
+                direction: 'none',
+                random: false,
+                straight: false,
+                outModes: {
+                  default: 'out',
+                },
+                attract: {
+                  enable: false,
+                  rotate: {
+                    x: 600,
+                    y: 1200,
+                  },
                 },
               },
             },
-          },
-          interactivity: {
-            detectsOn: 'canvas',
-            events: {
-              onDiv: {
-                selectors: '#repulse-div',
-                mode: 'repulse',
+            interactivity: {
+              detectsOn: 'canvas',
+              events: {
+                onDiv: {
+                  selectors: '#repulse-div',
+                  mode: 'repulse',
+                },
+                onHover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+                onClick: {
+                  enable: false,
+                  mode: 'push',
+                },
               },
-              onHover: {
-                enable: true,
-                mode: 'repulse',
-              },
-              onClick: {
-                enable: false,
-                mode: 'push',
+              modes: {
+                repulse: {
+                  distance: 100,
+                  duration: 1,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
               },
             },
-            modes: {
-              repulse: {
-                distance: 100,
-                duration: 1,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
-          retina_detect: true,
-        }}
-      />
+            retina_detect: true,
+          }}
+        />
+      </motion.div>
     );
   }
   return null;
